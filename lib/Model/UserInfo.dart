19bf40ecad319574c23fb9bf573271c1571b5cd9
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-26 20:11:52 +0500

    MFY auto commit at 26/12/2021 20:07:05

---
+++ lib/Model/UserInfo.dart
@@ -0,0 +1,43 @@
+import 'package:fb_login_app/Services/DB_Services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserInfoModel {
+  late final String name;
+  late final String email;
+  late final String uid;
+  late String profilePic = "/Profile Pics/download.jfif";
+
+  UserInfoModel({required this.name, required this.email, required this.uid});
+
+  UserInfoModel.secondInfo(
+      String kname, String kemail, String kuid, String kprofilePic) {
+    name = kname;
+    email = kemail;
+    uid = kuid;
+    profilePic = kprofilePic;
+  }
+
+  UserInfoModel.thirdInfo(String uid){
+    this.uid = uid;
+    getUser();
+  }
+
+  StoreUserInfor() {
+    Map<String, String> data = {
+      "name": this.name,
+      "email": this.email,
+      "uid": this.uid,
+      "profilePic": this.profilePic
+    };
+
+    DB_Services().storeUser(data);
+  }
+
+  getUser() async {
+    var data = await DB_Services().getUserData(uid);
+
+    name = data["name"];
+    email = data["email"];
+    profilePic = data["profilePic"];
+  }
+}
