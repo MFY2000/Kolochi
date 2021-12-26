25ca53b6dd5782cd3dbec9c77c28cb73b5f620b1
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-26 20:12:02 +0500

    MFY auto commit at 26/12/2021 20:07:05

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fb_login_app/Config/theme.dart';
+import 'package:fb_login_app/Pages/Home/MainScreen.dart';
+import 'package:fb_login_app/Pages/Splash/Splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'KOLACHI',
+      theme: theme(),
+      home: const SplashScreen(),
+    );
+  }
+}
+
