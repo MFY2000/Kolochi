9dc7005b5e12cd2dc58d2ad2ac2bc5d18e01b18d
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-17 16:26:29 +0500

    MFY auto commit at 17/01/2022 16:25:38

---
+++ lib/Components/Menu/MenuItem.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MenuScreen extends StatefulWidget {
+
+  @override
+  _MenuScreenState createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  final widthBox = SizedBox(
+    width: 16.0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+      ),
+    );
+  }
+}
+
+class MenuItemWidget extends StatelessWidget {
+  final MenuItem? item;
+  final Widget? widthBox;
+  final TextStyle? style;
+  final Function? callback;
+  final bool? selected;
+
+  final white = Colors.white;
+
+  const MenuItemWidget({
+    Key? key,
+    this.item,
+    this.widthBox,
+    this.style,
+    this.callback,
+    this.selected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => callback!(item!.index),
+      style: TextButton.styleFrom(
+        primary: selected! ? Color(0x44000000) : null,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            item!.icon,
+            color: white,
+            size: 24,
+          ),
+          widthBox!,
+          Expanded(
+            child: Text(
+              item!.title,
+              style: style,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MenuItem {
+  final String title;
+  final IconData icon;
+  final int index;
+
+  const MenuItem(this.title, this.icon, this.index);
+}
