d9ee6f764a1e85a46cfde7622c49ae597056ec44
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-17 16:26:42 +0500

    MFY auto commit at 17/01/2022 16:25:38

--- lib/Components/Custom/Cards/Product/ProductCard_1.dart
+++ lib/Components/Custom/Cards/Product/ProductCard_1.dart
@@ -1,30 +1,64 @@
+import 'package:fb_login_app/Components/Custom/Function/FunctionFactory.dart';
+import 'package:fb_login_app/Components/Custom/Rating/Rating.dart';
 import 'package:fb_login_app/Config/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:fb_login_app/Model/ModelClasses.dart';
+import 'package:fb_login_app/Pages/Details/ProductDetails.dart';
 
 class ProductCard_1 extends StatelessWidget {
-  const ProductCard_1({Key? key}) : super(key: key);
+  final ProductModel item;
+  const ProductCard_1({Key? key, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: getSize(false, 0.32),
-      width: getSize(true, 0.45),
-      child: Card(
-        child: Column(
-          children: [
-            Container(
-              height: getSize(false, 0.225),
-              color: Colors.black,
-            ),
-            Row(
-              children: [
-                for (var i = 0; i < 5; i++) 
-                  Icon(Icons.star)
-              ],
-
-            ),
-              Text("height: getSize(false, 0.3),")
-          ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ProductDetails(details: item, selected: cartSelected(item.pid),)),
+        );
+      },
+      child: SizedBox(
+        width: getSize(true, 0.425),
+        child: Card(
+          child: Column(
+            children: [
+              Container(
+                width: getSize(true, 0.425),
+                height: getSize(false, 0.25),
+                child: item.isAssets
+                    ? Image.asset(
+                        item.image,
+                        fit: BoxFit.fill,
+                      )
+                    : Image.network(
+                        item.image,
+                        fit: BoxFit.fill,
+                      ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(7),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("\$ ${item.price}"),
+                        Rating(count: item.rating),
+                      ],
+                    ),
+                    SizedBox(
+                      height: getSize(false, 0.0125),
+                    ),
+                    Text(item.title)
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
