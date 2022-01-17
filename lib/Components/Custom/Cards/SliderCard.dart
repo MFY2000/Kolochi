f66fe3ae7d0d11a8578877f17fc9ca1a0a806d66
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-17 16:26:42 +0500

    MFY auto commit at 17/01/2022 16:25:38

--- lib/Components/Custom/Cards/SliderCard.dart
+++ lib/Components/Custom/Cards/SliderCard.dart
@@ -1,42 +1,67 @@
-import 'package:fb_login_app/Components/Custom/Button/ButtonColored.dart';
+import 'package:fb_login_app/Config/constants.dart';
 import 'package:fb_login_app/Config/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:fb_login_app/Model/SliderPromo.dart';
 
 class SliderCard extends StatelessWidget {
-  const SliderCard({Key? key}) : super(key: key);
+  final SliderPromo item;
+  const SliderCard({Key? key, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Container(
+      child: SizedBox(
         height: getSize(false, .215),
         width: getSize(true, .91785),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
-              margin: EdgeInsets.all(4),
+              margin: const EdgeInsets.all(4),
+              padding: const EdgeInsets.all(4),
               width: getSize(true, .25),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Hallowen",
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      item.title,
+                      style:
+                          const TextStyle(color: khighLightColor, fontSize: 10),
+                    ),
                   ),
                   Text(
-                    "HallowenHallowenHallowenHallowen",
+                    item.subTitle,
+                    style: const TextStyle(color: kHeading, fontSize: 12),
                   ),
-                  Container(child: Text("Button")),
+                  Container(
+                      margin: const EdgeInsets.only(top: 18),
+                      padding: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                        gradient: kPrimaryGradientColor,
+                      ),
+                      child: Text(item.button,
+                          style: const TextStyle(
+                              color: kPrimaryLightColor,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w900))),
                 ],
               ),
             ),
-            Container(
+            SizedBox(
+              width: getSize(true, .45),
               height: getSize(false, .215),
-              child: Image.asset(
-                "assets/images/Smaple.png",
-                fit: BoxFit.fill,
-              ),
+              child: item.isAssets
+                  ? Image.asset(
+                      item.image,
+                      fit: BoxFit.fill,
+                    )
+                  : Image.network(
+                      item.image,
+                      fit: BoxFit.fill,
+                    ),
             ),
           ],
         ),
