223cdc577e68f9d73785a6530c03823f5ad8e796
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-17 16:26:44 +0500

    MFY auto commit at 17/01/2022 16:25:38

--- lib/Pages/Home/Components/HomeBody.dart
+++ lib/Pages/Home/Components/HomeBody.dart
@@ -1,6 +1,8 @@
-import 'package:fb_login_app/Components/Custom/Cards/Product/ProductCard_1.dart';
-import 'package:fb_login_app/Components/Custom/Cards/SliderCard.dart';
 import 'package:fb_login_app/Config/size_config.dart';
+import 'package:fb_login_app/Pages/Home/Components/ProductDisplay/GirdProduct.dart';
+import 'package:fb_login_app/Pages/Home/Components/ProductDisplay/HeaderHeading.dart';
+import 'package:fb_login_app/Pages/Home/Components/ProductDisplay/TopBanner.dart';
+import 'package:fb_login_app/Pages/Home/Components/Search/SearchFeild.dart';
 import 'package:fb_login_app/Pages/Home/Components/Slider/SliderList.dart';
 import 'package:flutter/material.dart';
 
@@ -14,11 +16,25 @@ class HomeBody extends StatefulWidget {
 class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SingleChildScrollView(
       child: Column(
         children: [
+          SearchFeild(),
           SliderList(),
-          ProductCard_1()
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: getSize(true, 0.05)),
+            child: Column(
+              children: const [
+                HeaderHeading(heading: "Featured"),
+                TopBanner(),
+                HeaderHeading(heading: "Best Sell"),
+                GirdProduct(),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: getSize(false, .1),
+          )
         ],
       ),
     );
