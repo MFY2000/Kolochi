bac49651aa8ae9ee19ed6d7edb2ae218d495e50d
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-17 16:26:20 +0500

    MFY auto commit at 17/01/2022 16:25:38

---
+++ lib/Components/Custom/Button/CounterButton.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:fb_login_app/Config/constants.dart';
+import 'package:fb_login_app/Config/size_config.dart';
+
+class CounterButton extends StatefulWidget {
+  final num startingValue;
+  final onChange;
+  const CounterButton({Key? key, required this.startingValue,required this.onChange}) : super(key: key);
+
+  @override
+  _CounterButtonState createState() => _CounterButtonState();
+}
+
+class _CounterButtonState extends State<CounterButton> {
+  num count = 0;
+@override
+  void initState() {
+    // TODO: implement initState
+    count = widget.startingValue;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: kSecondaryColor2,
+      width: getSize(true, 0.35),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+              onPressed: () {
+                counterChange(false);
+              },
+              icon: Icon(Icons.add)),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text("$count"),
+            decoration: BoxDecoration(border: Border.all(color: kPrimaryLightColor, width: 5)),
+          ),
+          IconButton(
+              onPressed: () {
+                counterChange(true);
+              },
+              icon: Icon(Icons.remove))
+        ],
+      ),
+    );
+  }
+
+  void counterChange(bool isAdded) {
+    bool condition = isAdded ? count > 1 : true;
+    if (condition) {
+      setState(() {
+        count = isAdded ? --count : ++count;
+        widget.onChange(count);
+      });
+    }
+  }
+}
