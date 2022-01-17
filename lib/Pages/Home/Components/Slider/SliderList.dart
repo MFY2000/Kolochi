21aac5f99d31afb6255e6c7484daffca6d8451cf
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-17 16:26:45 +0500

    MFY auto commit at 17/01/2022 16:25:38

--- lib/Pages/Home/Components/Slider/SliderList.dart
+++ lib/Pages/Home/Components/Slider/SliderList.dart
@@ -1,7 +1,9 @@
 import 'package:fb_login_app/Components/Custom/Cards/SliderCard.dart';
+import 'package:fb_login_app/Config/constants.dart';
 import 'package:fb_login_app/Config/size_config.dart';
-import 'package:fb_login_app/Pages/Home/Components/Slider/SliderIndicate.dart';
+import 'package:fb_login_app/Model/SliderPromo.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class SliderList extends StatefulWidget {
   const SliderList({Key? key}) : super(key: key);
@@ -11,33 +13,52 @@ class SliderList extends StatefulWidget {
 }
 
 class _SliderListState extends State<SliderList> {
+  final controller = PageController(viewportFraction: 0.8, keepPage: true);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: getSize(false, .215),
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                    margin:
-                        EdgeInsets.symmetric(horizontal: getSize(false, .01)),
-                    child: SliderCard());
-              }),
+    final pages = List.generate(
+        sliderList.length,
+        (index) => Container(
+            margin: EdgeInsets.symmetric(horizontal: getSize(false, .01)),
+            child: SliderCard(
+              item: sliderList[index],
+            )));
+
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      SizedBox(
+        height: getSize(false, .215),
+        child: PageView.builder(
+          controller: controller,
+          // itemCount: pages.length,
+          itemBuilder: (context, index) {
+            return pages[index % pages.length];
+          },
         ),
-        Container(
-          height: 10,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return SliderIndicate();
-                }))
-      ],
-    );
+      ),
+
+      // SmoothPageIndicator(
+      //   controller: controller,
+      //   count: pages.length,
+      //   effect: WormEffect(
+      //     dotHeight: 5,
+      //     dotWidth: 10,
+      //     type: WormType.thin,
+      //     // strokeWidth: 5,
+      //   ),
+      // ),
+      SmoothPageIndicator(
+        controller: controller,
+        count: pages.length,
+        effect: const JumpingDotEffect(
+          dotHeight: 5,
+          dotWidth: 10,
+          jumpScale: .7,
+          verticalOffset: 15,
+          dotColor: kSecondaryColor,
+          activeDotColor: kPrimaryColor,
+        ),
+      ),
+    ]);
   }
 }
