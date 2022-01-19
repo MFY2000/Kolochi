eed35647740ce33b04e40f14b7a27856b798a411
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-19 22:07:50 +0500

    MFY auto commit at 19/01/2022 22:06:50

--- lib/Pages/Cart/OrderDetails.dart
+++ lib/Pages/Cart/OrderDetails.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: file_names
 
+import 'package:fb_login_app/API/API_Route.dart';
+import 'package:fb_login_app/API/getApi.dart';
+import 'package:fb_login_app/Model/ModelClasses.dart';
+import 'package:fb_login_app/Pages/Cart/CartDetails.dart';
 import 'package:flutter/material.dart';
 import 'package:fb_login_app/Components/Custom/Button/ButtonColored.dart';
 import 'package:fb_login_app/Components/Custom/Cards/Cart/CartCard.dart';
@@ -9,46 +13,83 @@ import 'package:fb_login_app/Pages/Details/ProductDetails.dart';
 import 'package:fb_login_app/Pages/Profile/Address/AddressScreen.dart';
 import 'package:fb_login_app/Pages/Profile/Address/CreateAddress.dart';
 
-class OrderDetails extends StatefulWidget {
-  const OrderDetails({Key? key}) : super(key: key);
+class OrderDetailsScreen extends StatefulWidget {
+  const OrderDetailsScreen({Key? key}) : super(key: key);
 
   @override
-  OrderDetailsState createState() => OrderDetailsState();
+  OrderDetailsScreenState createState() => OrderDetailsScreenState();
 }
 
-class OrderDetailsState extends State<OrderDetails> {
+class OrderDetailsScreenState extends State<OrderDetailsScreen> {
+  List<CartsDetailsModel> orders = [];
+  bool isDataRecive = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getOrders();
+  }
+
+  getOrders() async {
+    var data = await getApi(api_GET_Orders + currentUser.id);
+
+    if (data != null) {
+      CartsDetailsModel temp;
+      for (var iData in data) {
+      print(iData);
+        for (var item in iData["products"]) {
+          temp = CartsDetailsModel(
+              item["productId"], item["quantity"], item["size"], item["color"]);
+          temp.subTitle = iData["status"];
+          orders.add(temp);
+        }
+      }
+    }
+    print("fewwfrr");
+    setState(() {
+      isDataRecive = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.centerLeft,
-      padding: EdgeInsets.symmetric(
-          vertical: getSize(false, .025), horizontal: getSize(true, .05)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Cart",
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          SizedBox(
-            height: getSize(true, .075),
-          ),
-          cartList.isNotEmpty
-              ? ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: cartList.length,
-                  itemBuilder: (context, index) => CartCard(
-                      item: cartList[index],
-                      onCounterChange: (int count) {},
-                      onRemove: () => {},
-                      onClick: () => {}),
-                )
-              : const Text("No Item in the cart."),
-        ],
-      ),
-    );
+    return isDataRecive
+        ? SingleChildScrollView(
+          child: Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(
+                  vertical: getSize(false, .025), horizontal: getSize(true, .05)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Ny Order",
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                  SizedBox(
+                    height: getSize(true, .075),
+                  ),
+                  orders.isNotEmpty
+                      ? ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: orders.length,
+                          itemBuilder: (context, index) => CartCard(
+                              item: orders[index],
+                              onCounterChange: (int count) {},
+                              onRemove: () => {},
+                              onClick: () => {}),
+                        )
+                      : const Text("No Item in the cart."),
+                ],
+              ),
+            ),
+        )
+        : const Center(
+            child: CircularProgressIndicator(),
+          );
   }
 }
