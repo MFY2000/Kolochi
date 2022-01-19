35cd6da84eb9f3caa1c14a4c050f832996e79353
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-19 21:28:30 +0500

    MFY auto commit at 19/01/2022 19:55:37

--- lib/Pages/Home/Components/Search/SearchFeild.dart
+++ lib/Pages/Home/Components/Search/SearchFeild.dart
@@ -1,14 +1,25 @@
+import 'package:fb_login_app/Config/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:fb_login_app/Config/size_config.dart';
 
 class SearchFeild extends StatelessWidget {
-  const SearchFeild({Key? key}) : super(key: key);
+  final onChange;
+  const SearchFeild({Key? key,required this.onChange}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.blue, borderRadius: BorderRadius.circular(100)),
+          color: kPrimaryLightColor,
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.25),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: const Offset(0, 14),
+            ),
+          ]),
       margin: EdgeInsets.symmetric(
           vertical: getSize(false, 0.025), horizontal: getSize(true, 0.015)),
       child: Container(
@@ -23,6 +34,7 @@ class SearchFeild extends StatelessWidget {
             focusedBorder: InputBorder.none,
             enabledBorder: InputBorder.none,
           ),
+          onChanged: onChange,
         ),
       ),
     );
